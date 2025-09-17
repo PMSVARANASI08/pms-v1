f024c8991b76362f17ed56ec126e76ce4c5ff7e1
Deepak Singh <DSDEEPAK1792@GMAIL.COM>
2025-09-17 22:54:56 +0530

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -2,7 +2,7 @@ export const SCHOOL = {
   FULL_NAME: 'Police Modern School',
   SHORT_NAME: 'PMS',
   SHORT_ADDRESS: '34 BATTALION, PAC VARANASI',
-  ADDRESS: 'S34 Battalion, PAC Campus, Bhullanpur, Varanasi, Uttar Pradesh',
+  ADDRESS: '34 Battalion, PAC Campus, Bhullanpur, Varanasi, Uttar Pradesh',
   EMAIL: 'pms34bnvns@gmail.com',
   PHONE: '+91 9235473662',
   LOCATION_COORDINATES: '25.287972,82.944944'
